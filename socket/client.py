68b91a7019343ae3fe397daccb20918a98921919
gotoERROR00111011 <gotoerror00111011@gmail.com>
2019-08-02 06:52:41 +0900

    Add files via upload

---
+++ socket/client.py
@@ -0,0 +1,36 @@
+import cv2
+import io
+import socket
+import struct
+import pickle
+
+ip = ''
+username = ''
+password = ''
+
+IP = ip
+PORT = 8485
+img_counter = 0
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+connection = client_socket.makefile('wb')
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 320);
+cam.set(4, 240);
+
+img_counter = 0
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+    ret, frame = cam.read()
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    data = pickle.dumps(frame, 0)
+    size = len(data)
+
+    print("{}: {}".format(img_counter, size))
+    client_socket.sendall(struct.pack(">L", size) + data)
+    img_counter += 1
+
+cam.release()
